259f5106e2e7a86215414a8045769414e930e799
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-08-28 11:22:58 -0400

    Initial code

---
+++ README.md
@@ -0,0 +1,1 @@
+Code from **[Programming Machine Learning](https://pragprog.com/titles/pplearn/programming-machine-learning/)** by Paolo Perrotta, translated from Python to Elixir for practice.
---
+++ lib/ml.ex
@@ -0,0 +1,18 @@
+defmodule ML do
+  @moduledoc """
+  Documentation for `ML`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ML.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
